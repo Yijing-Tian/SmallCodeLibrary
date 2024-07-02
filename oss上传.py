74367b30c83ca35f69a47e80731a1ce32fe9524d
LL02 <69142536+LL020202@users.noreply.github.com>
2024-07-02 15:30:36 +0800

    Update oss上传.py

--- oss上传.py
+++ oss上传.py
@@ -4,10 +4,10 @@ pip install oss2
 
 
 import oss2
-auth = oss2.Auth('LTAIeOayNW4ZjgrY', '6cAhgzBXg1cvGrdek2sgdXz8Ws9Ppq')
+auth = oss2.Auth('LTAI******', '6cAh******')
 endpoint = 'http://oss-cn-beijing.aliyuncs.com'
-bucket = oss2.Bucket(auth, endpoint, 'yundao888') 
+bucket = oss2.Bucket(auth, endpoint, 'password') 
 
 save_path = 'data/'+str(random.randint(0,100))+str(int(time.time()*1000000))+'.jpg'
-bucket.put_object_from_file('H5-RES/2022/mxgc/'+save_path, save_path)
+bucket.put_object_from_file('save_dir/'+save_path, save_path)
 
